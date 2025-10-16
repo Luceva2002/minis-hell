d9576302b5a5ff85211e2edcb18d3d5d331822a5
Lina Gharbia <ligharbi@e3r2p1.42roma.it>
2025-10-16 22:43:39 +0200

    iniziamo

--- includes/minishell.h
+++ includes/minishell.h
@@ -10,3 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef	MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include "libft.h"
+#include "ft_printf.h"
+#include "get_next_line_bonus.h"
+#include "parser.h"
+
+
+#endif
